014c8d5e0ebf0bac31c580608714728fe3f779db
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-09-15 19:33:16 -0300

    anotação para endereçar o Servlet

--- src/main/java/com/github/lucasdevrj/bombafilmes/servlet/OiMundoServlet.java
+++ src/main/java/com/github/lucasdevrj/bombafilmes/servlet/OiMundoServlet.java
@@ -1,5 +1,20 @@
 package com.github.lucasdevrj.bombafilmes.servlet;
 
-public class OiMundoServlet {
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/oi")
+public class OiMundoServlet extends HttpServlet {
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) {
+		
+		
+		
+	}
 }
