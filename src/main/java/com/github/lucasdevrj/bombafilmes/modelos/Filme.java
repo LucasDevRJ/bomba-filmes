bb83a32089b410787e46efee6a9c67126b8d35b1
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-09-16 20:43:55 -0300

    atributos do filme

--- src/main/java/com/github/lucasdevrj/bombafilmes/modelos/Filme.java
+++ src/main/java/com/github/lucasdevrj/bombafilmes/modelos/Filme.java
@@ -2,4 +2,10 @@ package com.github.lucasdevrj.bombafilmes.modelos;
 
 public class Filme {
 
+	private String nome;
+	private String sinopse;
+	private Integer faixaEtaria;
+	private String genero;
+	private String elenco;
+	private String urlImagem;
 }
