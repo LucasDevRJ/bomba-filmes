081a927c74fd5ae538dad4eaa4101b86ab5f8efa
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-09-16 19:36:05 -0300

    armazenando parâmetro do filme

--- src/main/java/com/github/lucasdevrj/bombafilmes/servlet/NovoFilmeServlet.java
+++ src/main/java/com/github/lucasdevrj/bombafilmes/servlet/NovoFilmeServlet.java
@@ -1,24 +1,31 @@
 package com.github.lucasdevrj.bombafilmes.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class NovoFilmeServlet
- */
+
 @WebServlet("/novoFilme")
 public class NovoFilmeServlet extends HttpServlet {
+	
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		System.out.println("Cadastrando novo filme");
+		
+		String nomeFilme = request.getParameter("nome");
+		
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<h1>Filme " + nomeFilme + " Cadastrado com Sucesso!</h1>");
+		out.println("</body>");
+		out.println("</html>");
 	}
 
 }
