202bacace3c90e4965a9618dcd20508758f0ec24
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-09-17 20:31:36 -0300

    exibindo atributos do filme

--- src/main/java/com/github/lucasdevrj/bombafilmes/servlet/ListaFilmesServlet.java
+++ src/main/java/com/github/lucasdevrj/bombafilmes/servlet/ListaFilmesServlet.java
@@ -1,6 +1,7 @@
 package com.github.lucasdevrj.bombafilmes.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -19,8 +20,22 @@ public class ListaFilmesServlet extends HttpServlet {
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
+		
 		BancoDeDados bancoDeDados = new BancoDeDados();
 		List<Filme> filmes = bancoDeDados.getFilmes();
+		PrintWriter out = response.getWriter();
+		
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<ul>");
+		
+		for (Filme filme : filmes) {
+			out.println("<li>" + filme.getNome() + "</li>");
+		}
+		
+		out.println("</ul>");
+		out.println("</body>");
+		out.println("</html>");
 	}
 
 }
