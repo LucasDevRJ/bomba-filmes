d0abf07d04d0608bae7078bfd1000d7172622d6f
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-09-30 21:50:55 -0300

    Classe para executar ações via parâmetro

--- src/main/java/com/github/lucasdevrj/bombafilmes/servlet/UnicaEntradaServlet.java
+++ src/main/java/com/github/lucasdevrj/bombafilmes/servlet/UnicaEntradaServlet.java
@@ -7,18 +7,26 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class UnicaEntradaServlet
- */
 @WebServlet("/entrada")
 public class UnicaEntradaServlet extends HttpServlet {
+	
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	protected void service(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		
+		String parametroAcao = request.getParameter("acao");
+		
+		if (parametroAcao.equals("catalogo")) {
+			System.out.println("Exibindo catalogo");
+		} else if (parametroAcao.equals("cadastro")) {
+			System.out.println("Cadastrando Filme");
+		} else if (parametroAcao.equals("removeFilme")) {
+			System.out.println("Removendo Filme");
+		} else if (parametroAcao.equals("exibeFilme")) {
+			System.out.println("Exibindo Filme");
+		} else if (parametroAcao.equals("editaFilme")) {
+			System.out.println("Editando Filme");
+		}
 	}
-
 }
