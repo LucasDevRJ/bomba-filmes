797b5faac3e3c2828fab48f67f988dd51b81b640
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-10-06 08:54:26 -0300

    método para deslogar da aplicação

--- src/main/java/com/github/lucasdevrj/bombafilmes/acoes/Logout.java
+++ src/main/java/com/github/lucasdevrj/bombafilmes/acoes/Logout.java
@@ -1,5 +1,22 @@
 package com.github.lucasdevrj.bombafilmes.acoes;
 
-public class Logout {
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class Logout implements Acao {
+
+	@Override
+	public String executa(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		HttpSession sessao = request.getSession();
+		sessao.removeAttribute("usuarioLogado");
+	
+		return "redirect:entrada?acao=ExibirFormularioLogin";
+	}
 
 }
