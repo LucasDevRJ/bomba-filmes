67b4c3dce7e0740be3d46bb576e82bf5fe5b5f9d
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-10-07 10:48:44 -0300

    criação do Servlet do Catalogo

---
+++ src/main/java/com/github/lucasdevrj/bombafilmes/servlet/CatalogoService.java
@@ -0,0 +1,21 @@
+package com.github.lucasdevrj.bombafilmes.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/catalogoService")
+public class CatalogoService extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+}
