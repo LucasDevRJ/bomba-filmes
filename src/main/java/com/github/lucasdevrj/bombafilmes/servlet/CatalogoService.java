de5fe6c02b04cb213b1a2ea4885e801c80d0faf3
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-10-07 11:05:51 -0300

    usando Json para consumir dados

--- src/main/java/com/github/lucasdevrj/bombafilmes/servlet/CatalogoService.java
+++ src/main/java/com/github/lucasdevrj/bombafilmes/servlet/CatalogoService.java
@@ -1,11 +1,17 @@
 package com.github.lucasdevrj.bombafilmes.servlet;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.github.lucasdevrj.bombafilmes.modelos.BancoDeDados;
+import com.github.lucasdevrj.bombafilmes.modelos.Filme;
+import com.google.gson.Gson;
 
 @WebServlet("/catalogoService")
 public class CatalogoService extends HttpServlet {
@@ -14,8 +20,14 @@ public class CatalogoService extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		List<Filme> catalogo = new BancoDeDados().getFilmes();
+		
+		Gson gson = new Gson();
+		String json = gson.toJson(catalogo);
+		
+		response.setContentType("application/json");
+		response.getWriter().println(json);
 	}
 
 }
